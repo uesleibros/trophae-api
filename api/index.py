fd5b79126f7e15d969fc706e159cbe9f5d7b7735
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-06-11 21:02:09 -0300

    alterado estrutura

--- api/index.py
+++ api/index.py
@@ -1,7 +1,26 @@
-from app import create_app
-from flask import Flask
+from flask import Flask, g
+from config import Config
+from extensions.database import Database
+from main import main_blueprint
+from auth import auth_blueprint
+
+def create_app() -> Flask:
+   app = Flask(__name__)
+   config: Config = Config()
+   app.config.from_object(config)
+
+   database: Database = Database(config)
+
+   @app.before_request
+   def before_request():
+      g.database: Database = database
+
+   app.register_blueprint(main_blueprint, url_prefix="/")
+   app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
+   return app
 
 app: Flask = create_app()
 
 if __name__ == "__main__":
-	app.run()
+	app.run(port=4000, host="0.0.0.0", debug=True)
